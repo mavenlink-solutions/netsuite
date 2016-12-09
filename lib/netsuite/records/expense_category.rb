f82034b2c571ddad1918ad109535d2f5d22a1ccf
Shirish Pampoorickal <shirish@mavenlink.com>
2016-12-09 11:50:49 -0800

    Add record URL and search_class_name to ExpenseCategory

--- lib/netsuite/records/expense_category.rb
+++ lib/netsuite/records/expense_category.rb
@@ -5,6 +5,8 @@ module NetSuite
       include Support::Actions
       include Namespaces::ListAcct
 
+      # https://system.netsuite.com/help/helpcenter/en_US/srbrowser/Browser2014_1/script/record/expensecategory.html
+
       actions :search
 
       fields :name
@@ -14,6 +16,10 @@ module NetSuite
       def initialize(attributes = {})
         @internal_id = attributes.delete(:internal_id) || attributes.delete(:@internal_id)
         initialize_from_attributes_hash(attributes)
+      end
+
+      def self.search_class_name
+        'ExpenseCategory'
       end
     end
   end
