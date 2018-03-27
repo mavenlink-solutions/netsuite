84e43ecab64b3ce4f912f8cffd0b0c0da71a55b0
arincon9 <arincon@mavenlink.com>
2018-03-27 15:36:41 -0500

    Add attributes to message media item list

--- lib/netsuite/records/message_media_item_list.rb
+++ lib/netsuite/records/message_media_item_list.rb
@@ -1,9 +1,19 @@
 module NetSuite
   module Records
     class MessageMediaItemList
+      include Support::Fields
+      include Support::RecordRefs
+      include Support::Records
       include Namespaces::CommGeneral
 
-      fields :mediaItem
+      fields :media_item
+
+      attr_accessor :replace_all
+
+      def initialize(attributes = {})
+        @replace_all = attributes.delete(:replace_all) || attributes.delete(:replace_all)
+        initialize_from_attributes_hash(attributes)
+      end
     end
   end
 end
